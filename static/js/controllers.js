05eaa26ac338d487948f3abe5ce5f46420242b57
Orry Baram <orrybaram@gmail.com>
2013-10-25 23:45:35 -0400

    added basic voting

--- static/js/controllers.js
+++ static/js/controllers.js
@@ -6,10 +6,22 @@ angular.module('app.controllers', []).
   controller('MainCtrl', ['$scope', '$http', function($scope, $http) {
     
     $scope.tests = [];
+    $scope.voted_for_a = false;
+    $scope.voted_for_b = false;
     
     $http.get('/tests/').success(function(data){
       $scope.tests = data;
     })
+
+    $scope.vote_for_a = function() {
+      $scope.voted_for_a = true;
+      $scope.voted_for_b = false;
+    }
+
+    $scope.vote_for_b = function() {
+      $scope.voted_for_a = false;
+      $scope.voted_for_b = true;
+    }
 
   }]).
 
